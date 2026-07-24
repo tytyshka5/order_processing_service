fe0c6f57ea6b0afa49c6a56567dd2b299f22fc5d
tytyshka <shobyrevdan@gmail.com>
2026-07-24 17:20:21 +0300

    Refactoring

--- cmd/order-service/main.go
+++ cmd/order-service/main.go
@@ -2,34 +2,47 @@ package main
 
 import (
 	"fmt"
-	"github.com/tytyshka5/order-processing-service/internal/domain"
+	"order_processing_service/internal/domain"
 	"time"
 )
 
 func main() {
 
-	Computer := domain.Product{
+	computer := domain.Product{
 		ID:           1,
 		Name:         "Omni",
 		PriceKopecks: 19999,
 		Stock:        13,
 	}
 
-	ComputerItem := domain.OrderItem{
-		ProductID: Computer.ID,
-		Quntity:   2,
-		UnitPrice: Computer.PriceKopecks,
+	mouse := domain.Product{
+		ID:           2,
+		Name:         "Logitech",
+		PriceKopecks: 1999,
+		Stock:        10,
 	}
 
-	ComputerOrder := domain.Order{
+	mouseItem := domain.OrderItem{
+		ProductID: mouse.ID,
+		Quantity:  3,
+		UnitPrice: mouse.PriceKopecks,
+	}
+
+	computerItem := domain.OrderItem{
+		ProductID: computer.ID,
+		Quantity:  2,
+		UnitPrice: computer.PriceKopecks,
+	}
+
+	Order := domain.Order{
 		ID:         1,
 		CustomerID: 1,
-		Items:      []domain.OrderItem{ComputerItem},
+		Items:      []domain.OrderItem{computerItem, mouseItem},
 		Status:     domain.StatusNew,
 		CreatedAt:  time.Now(),
 	}
 
-	orderSum := ComputerOrder.OrderPrice()
+	orderSum := Order.Total()
 
-	fmt.Printf("order=%d status=%s items=%d total=%d", ComputerOrder.ID, ComputerOrder.Status, len(ComputerOrder.Items), orderSum)
+	fmt.Printf("order=%d status=%s items=%d total=%d\n", Order.ID, Order.Status, len(Order.Items), orderSum)
 }
